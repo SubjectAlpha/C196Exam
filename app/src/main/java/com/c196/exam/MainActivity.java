723da33884a810cb8dd67012ba6fd02e16fb2f25
Jake Starr <jakestarr60@gmail.com>
2023-08-22 13:12:38 -0400

    FtS

--- app/src/main/java/com/c196/exam/MainActivity.java
+++ app/src/main/java/com/c196/exam/MainActivity.java
@@ -3,6 +3,7 @@ package com.c196.exam;
 import androidx.appcompat.app.AppCompatActivity;
 import androidx.appcompat.widget.Toolbar;
 
+import android.app.AlarmManager;
 import android.app.NotificationChannel;
 import android.app.NotificationManager;
 import android.content.Intent;
@@ -13,11 +14,15 @@ import android.view.View;
 import android.widget.LinearLayout;
 
 import com.c196.exam.database.DatabaseHelper;
+import com.c196.exam.entities.Assessment;
+import com.c196.exam.entities.Course;
 import com.c196.exam.entities.Term;
 import com.c196.exam.ui.dialogs.CreateTermDialogFragment;
 import com.c196.exam.ui.fragments.MainCardFragment;
 import com.c196.exam.utility.NotificationService;
 import com.google.android.material.floatingactionbutton.FloatingActionButton;
+
+import java.time.Instant;
 
 public class MainActivity extends AppCompatActivity {
 
@@ -46,7 +51,60 @@ public class MainActivity extends AppCompatActivity {
         LinearLayout ll = findViewById(R.id.main_layout);
 
         if(savedInstanceState == null){
+            AlarmManager am = (AlarmManager) getSystemService(ALARM_SERVICE);
+            NotificationService ns = new NotificationService();
             for ( Term t : dbh.getTerms() ) {
+
+                //Notification Logic
+                //If a term is coming up within a week, notify, same for class, and assessment
+                //If a term is ending within a week, notify
+
+                int secondsInAWeek = 604800;
+                Instant now = Instant.now();
+                Instant weekFromNow = now.plusSeconds(secondsInAWeek);
+                Instant termStart = Instant.parse(t.getStart());
+                Instant termEnd = Instant.parse(t.getEnd());
+
+                if(termStart.isBefore(now) && termStart.isAfter(weekFromNow)){
+                    String content = "Your term " + t.getTitle() + " begins next week!";
+                    ns.scheduleNotification(this, am,0, "Term Starting Soon", content);
+                }
+
+                if(termEnd.isBefore(now) && termEnd.isAfter(weekFromNow)){
+                    String content = "Your term " + t.getTitle() + " ends next week!";
+                    ns.scheduleNotification(this, am,0, "Term Ending Soon", content);
+                }
+
+                for(Course c : t.getCourses()){
+                    Instant courseStart = Instant.parse(c.getStart());
+                    Instant courseEnd = Instant.parse(c.getEnd());
+
+                    if(courseStart.isBefore(now) && courseStart.isAfter(weekFromNow)){
+                        String content = "Your course " + c.getTitle() + " begins next week!";
+                        ns.scheduleNotification(this, am,0, "Course Starting Soon", content);
+                    }
+
+                    if(courseEnd.isBefore(now) && courseEnd.isAfter(weekFromNow)){
+                        String content = "Your course " + c.getTitle() + " ends next week!";
+                        ns.scheduleNotification(this, am,0, "Course Ending Soon", content);
+                    }
+
+                    for(Assessment a : c.getAssessments()){
+                        Instant assessmentStart = Instant.parse(a.getStart() + "T00:00:00Z");
+                        Instant assessmentEnd = Instant.parse(a.getEnd() + "T00:00:00Z");
+
+                        if(assessmentStart.isBefore(now) && assessmentStart.isAfter(weekFromNow)){
+                            String content = "Your assessment " + a.getTitle() + " begins next week!";
+                            ns.scheduleNotification(this, am,0, "Assessment Opens Soon", content);
+                        }
+
+                        if(assessmentEnd.isBefore(now) && assessmentEnd.isAfter(weekFromNow)){
+                            String content = "Your assessment " + a.getTitle() + " ends next week!";
+                            ns.scheduleNotification(this, am,0, "Assessment Due Soon", content);
+                        }
+                    }
+                }
+
                 getSupportFragmentManager().beginTransaction().add(ll.getId(), MainCardFragment.newInstance(t)).commit();
             }
         }
--- app/src/main/java/com/c196/exam/utility/NotificationService.java
+++ app/src/main/java/com/c196/exam/utility/NotificationService.java
@@ -1,6 +1,9 @@
 package com.c196.exam.utility;
 
 import android.Manifest;
+import android.app.Activity;
+import android.app.AlarmManager;
+import android.app.PendingIntent;
 import android.content.BroadcastReceiver;
 import android.content.Context;
 import android.content.Intent;
@@ -15,6 +18,14 @@ public class NotificationService extends BroadcastReceiver {
     public static final String CHANNEL_NAME = "C196 Notification";
     public static final String CHANNEL_DESC = "C196 Exam Notifications";
 
+    public void scheduleNotification(Context c, AlarmManager a, long triggerTime, String title, String content) {
+        Intent i = new Intent(c, this.getClass());
+        i.putExtra("title", title);
+        i.putExtra("content", content);
+        PendingIntent pi = PendingIntent.getBroadcast(c, 0, i, PendingIntent.FLAG_IMMUTABLE);
+        a.set(AlarmManager.RTC_WAKEUP, triggerTime, pi);
+    }
+
     public void displayNotification(Context context, String title, String content) {
         NotificationCompat.Builder notification =
                 new NotificationCompat.Builder(context, CHANNEL_ID)
@@ -26,11 +37,11 @@ public class NotificationService extends BroadcastReceiver {
         if (ActivityCompat.checkSelfPermission(context, Manifest.permission.POST_NOTIFICATIONS) != PackageManager.PERMISSION_GRANTED) {
             return;
         }
-        mNotificationManager.notify(1, notification.build());
+        mNotificationManager.notify(Integer.MAX_VALUE, notification.build());
     }
 
     @Override
     public void onReceive(Context context, Intent intent) {
-
+        displayNotification(context, intent.getStringExtra("title"), intent.getStringExtra("content"));
     }
 }
